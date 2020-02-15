c14823b3e5d76f9877a7d550713e810cf91d85be
EgorChindyasov <egor@chindyasov.ru>
2020-02-15 16:24:35 +0300

    release v1

---
+++ src/components/Profile.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://ps.w.org/360-image/assets/banner-772x250.jpg?rev=1933038" />
+            </div>
+            <div>
+                avatar + description
+        </div>
+            <div>
+                My Post
+          <div>
+                    New Post
+        </div>
+            </div>
+            <div>
+                <div>
+                    Post 1
+        </div>
+                <div>
+                    Post 2
+        </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
