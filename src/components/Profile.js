917be792e92766d7b7d9db987496b4cd0a258426
EgorChindyasov <egor@chindyasov.ru>
2020-02-15 18:12:15 +0300

    realese v1

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={classes.content}>
             <div>
                 <img src="https://ps.w.org/360-image/assets/banner-772x250.jpg?rev=1933038" />
             </div>
